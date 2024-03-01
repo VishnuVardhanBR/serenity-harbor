67f97a85e62d78a1fc0d8c27c682422a678731b1
VishnuVardhanBR <mmsvishnu23@gmail.com>
2024-03-01 16:57:10 +0530

    minor style changes

--- frontend/src/components/HomePage/Howitworks.jsx
+++ frontend/src/components/HomePage/Howitworks.jsx
@@ -42,7 +42,7 @@ export default function Howitworks() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
         
-          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <p className="mt-12 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             How it Works
           </p>
           <p className="mt-6 text-lg leading-8 text-gray-600">
--- frontend/src/components/HomePage/NewHeader.jsx
+++ frontend/src/components/HomePage/NewHeader.jsx
@@ -128,7 +128,7 @@ export default function Header() {
             }}
           />
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="mx-auto max-w-2xl pt-32 st:py-48 lg:pt-56">
           
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
